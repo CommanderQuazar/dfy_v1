92e525417ababf7c9e643c2731a10a420f0b3597
Tobey Ragain <tobeyragain@gmail.com>
2021-11-14 02:17:29 -0800

    Created HashSet

--- HashMap.cpp
+++ HashMap.cpp
@@ -3,3 +3,39 @@
 //
 
 #include "HashMap.h"
+#include <memory>
+
+
+HashMap& HashMap::remove(const std::shared_ptr<File>& to_remove)
+{
+    auto ret = hash_set.find(to_remove);
+    if(ret == hash_set.end())
+        return *this;
+    else
+        hash_set.erase(to_remove);
+    return *this;
+}
+
+HashMap& HashMap::add(File &to_add)
+{
+    hash_set.insert(std::make_shared<File>(to_add));
+    return *this;
+}
+
+hash_set_type::iterator& HashMap::lookup(const std::shared_ptr<File>& term)
+{
+    auto ret = hash_set.find(term);
+    return ret;
+    //????
+}
+
+HashMap& HashMap::print_all()
+{
+    for(auto begin = hash_set.begin(); begin != hash_set.end(); )
+    {
+        std::cout << "Massage: "
+                  << (*begin++)->get_msg()
+                  << std::endl;
+    }
+    return *this;
+}
--- HashMap.h
+++ HashMap.h
@@ -5,9 +5,25 @@
 #ifndef DFY_V1_HASHMAP_H
 #define DFY_V1_HASHMAP_H
 
+#include "File.h"
+#include <iostream>
+#include <unordered_set>
+
+using hash_set_type = std::unordered_set<std::shared_ptr<File>>;
 
 class HashMap
 {
+    public:
+        HashMap() = default;
+        explicit HashMap(File& file_obj) { hash_set.insert(std::make_shared<File>(file_obj)); };
+
+        HashMap& add(File& to_add);
+        HashMap& remove(const std::shared_ptr<File>& to_remove);
+        hash_set_type::iterator& lookup(const std::shared_ptr<File>& term);
+        HashMap& print_all();
+
+    private:
+        hash_set_type hash_set;
 
 };
 
